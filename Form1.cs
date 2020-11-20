be75a668a4c727eba47e6fd43ee7cfc8ed3de5c3
bensm1 <49214651+bensm1@users.noreply.github.com>
2020-11-20 14:54:50 +0000

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        Tile[] tiles;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var previousPosition = new Point(0, 0);
+            var buttonSize = new Size(25, 25);
+
+            int rows = 12;
+            int columns = 13;
+
+            for (int i = 0; i <= columns; i++)
+            {
+                for (int j = 0; j <= rows; j++)
+                {
+                    var button = new Button
+                    {
+                        Name = $"Button{i}",
+                        Text = "",
+                        Location = previousPosition,
+                        Size = buttonSize
+                    };
+
+                    var tile = new Tile(previousPosition, false, button);
+
+                    // Move the next button by the size of the buttons.
+                    previousPosition.X += buttonSize.Width;
+
+                    if (previousPosition.X > ClientSize.Width + buttonSize.Width)
+                    {
+                        // New line.
+                        previousPosition.Y += buttonSize.Height;
+                        // Start from the beginning.
+                        previousPosition.X = 0;
+                    }
+
+                    Controls.Add(button);
+                }
+            }
+        }
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    class Tile
+    {
+        public Tile(Point position, bool isMine, Button button)
+        {
+            this.position = position;
+            this.isMine = isMine;
+            this.button = button;
+        }
+
+        Button button;
+        Point position;
+        bool isMine;
+    }
+}
